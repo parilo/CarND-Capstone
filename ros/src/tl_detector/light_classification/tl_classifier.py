f4ee0d9903a1c9ced4c6b0be3858d869fcd29d78
Andrew L. Ayers <junkbotix@gmail.com>
2017-09-18 23:47:24 -0700

    Updated color detection masks to better fit images; added ratio calculation

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -37,26 +37,36 @@ class TLClassifier(object):
         # http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_colorspaces/py_colorspaces.html
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        #low_red = np.array([30, 150, 50])
-        #upp_red = np.array([255, 255, 180])
-        low_red = np.array([179, 50, 128])
+        low_red = np.array([0, 25, 40])
         upp_red = np.array([179, 255, 255])
 
-        low_yel = np.array([25, 50, 128])
-        upp_yel = np.array([25, 255, 255])
+        low_yel = np.array([20, 160, 0])
+        upp_yel = np.array([30, 255, 255])
 
-        low_grn = np.array([60, 50, 128])
-        upp_grn = np.array([60, 255, 255])
+        low_grn = np.array([50, 179, 102])
+        upp_grn = np.array([75, 255, 255])
 
         mask = cv2.inRange(hsv, low_red, upp_red)
         res = cv2.bitwise_and(img, img, mask=mask)
 
-        #cv2.imshow('Image', npimage) <-- does not work inside docker container
+        # num of pixels in mask / image size
+        #
+        # image size is divided by 3.0 twice:
+        #
+        #   1. First, because size is # pixels * # planes,
+        #      we just want the number on a single plane
+        #
+        #   2. Second, because we want to know how much
+        #      area is taken up in the 1/3 of the space a
+        #      single "light" occupies
+        #
+        ratio = (cv2.countNonZero(mask) / ((img.size / 3.0) / 3.0))
 
         cv2.imwrite('./images/testing.jpg', res)
 
-        print img.shape
-        print img.size
-        print img.dtype
+        #print img.shape
+        #print img.size
+        #print img.dtype
+        print 'color percentage:', np.round(ratio * 100, 1), '%'
 
         return TrafficLight.UNKNOWN # GREEN, YELLOW, RED
